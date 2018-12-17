0bd21669f6ce4edadcb9483c8b01ee7b58540537
Ethan Roberts <ejrobe@gmail.com>
2018-12-17 21:47:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = Array.new()
+
+def take_a_number(arr, name)
+  arr.push(name)
+  
+  puts "Thank you for coming. Your number is #{arr.length}"
+end
+
+def now_serving(arr)
+  
+end
