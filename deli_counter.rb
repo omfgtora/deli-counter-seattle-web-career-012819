992fd7c5144ed7a561b8187a72ad1038c9fc879d
Ethan Roberts <ejrobe@gmail.com>
2018-12-17 22:24:52 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,6 @@ end
 
 def take_a_number(arr, name)
   arr << name
-  
   puts "Welcome, #{name}. You are number #{arr.length} in line."
 end
 
@@ -23,6 +22,6 @@ def now_serving(arr)
   if arr.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{arr.[0]}."
+    puts "Currently serving #{arr[0]}."
   end
 end
